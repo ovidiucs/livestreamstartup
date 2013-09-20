b48b2d1c02668a730b44e3def75591cad359f0bc
Constantinescu Ovidiu <ovidiu.ctin@gmail.com>
2013-09-20 11:53:01 +0000

    MVC start, watch out for commas, punctuation

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
-echo 'testing';
 // Controller
+// 1. What URL are they trying to access:
+
+//echo $_SERVER['REQUEST_URI'];
+// 2. If user is on the homepage ($_SERVER returns '/') then echo the proper message else say otherwise
+	if ($_SERVER['REQUEST_URI'] == '/') {
+		// HOMEPAGE
+		// Get the homepage from a file
+//		echo file_get_contents("../templates/index.html");
+		// Or print a message 
+//		 echo 'You are on the homepage';
+		// or str_replace to put in content in the html placeholders
+		$content = file_get_contents("../templates/index.dyn");
+		$content = str_replace  ("{text}","Replaced text", $content);
+		echo $content;
+	} else {
+		echo 'You are not on the homepage';
+		// NOT HOMEPAGE
+	}
+
+
+
+
