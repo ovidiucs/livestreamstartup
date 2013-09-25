f9dd200a581c974f9321e1cd31ed87e0ea59db73
Constantinescu Ovidiu <ovidiu.ctin@gmail.com>
2013-09-25 19:10:40 +0000

    single-row-query si multi-row-query, inainte de clasa de logging

--- classes/class.Database.php
+++ classes/class.Database.php
@@ -5,14 +5,14 @@
 			if ($this->database = mysqli_connect("localhost","root","protocolaritate","users")) {
 				echo "Connected <br />";
 				$sqlQuery = "SELECT * FROM users.users";
-				$result = $this->multiRowQuery($sqlQuery);
+					$result = $this->multiRowQuery($sqlQuery);
 				print_r($result);
 			} else {
 			echo "Could not connect\n";
 		}
 	}
 
-			function multiRowQuery($SqlQuery){
+			function multiRowQuery($sqlQuery){
 				$sqlQuery = "SELECT * FROM users.users";
 				$result    = $this->database->query($sqlQuery);
 
@@ -23,4 +23,36 @@
 			}
 				return $returnArray;
 		}
+			function singleRowQuery($sqlQuery){
+				$result = $this->database->query($sqlQuery);
+
+				$rows = array();
+				while ($rows = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+					return $rows;
+				}
+
+				//Ceva s-a intamplat
+				return 0;
+			}
+
 	}
+
+//	singleRowQuery returneaza:
+//	 (
+//		 [username] => carl
+//		 [password] => 123
+//	 )
+//
+//	multiRowQuery returneaza:
+//	(
+//		 [0] => Array
+//			 (
+//		 [username] => carl
+//		 [password] => 123
+//	   )
+//		 [1] => Array
+//		  (
+//		 [username] => carl2
+//		 [password] => 321
+//	   )
+//	) 
