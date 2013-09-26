35a266d0f32d15801e1ad9fd636fc4fcc92bf829
Constantinescu Ovidiu <ovidiu.ctin@gmail.com>
2013-09-26 19:40:43 +0000

    Regex for Parse URI

--- classes/class.ParseURI.php
+++ classes/class.ParseURI.php
@@ -2,6 +2,25 @@
 	// Clasa pentru parsarea URI-urilor
 	class TParseURI{
 		function __construct($uri) {
+            preg_match("!^/([^/]+)!imsx",$uri,$pmatches);
+		//	Array
+		//		(
+		//		     [0] => /jjjjj
+		//		     [1] => jjjjj
+		//		)
+		// exact name of the page and the URI		
+			
+			$className = $pmatches[0];
+			echo "Page name: ".$className."<br />";
+		// Securitate
+	   		if(strlen($className) > 32) {
+				// TODO:Logging Message
+				die("Unexpected Error");
+			}
+		// unexpected input or very large input can create buffer overflows
+			$className = preg_replace("![^a-z0-9]!imsx","",$className);
+		// preg_replace - daca nu are 09az  in nume page nu ne intereseaza
+			echo "Class Name: ".$className."<br />";
 
 			$this->pageName = '';
 			// Paseaza URI la construct si verifica posibilitati
